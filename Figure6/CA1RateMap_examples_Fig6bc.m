78f507099bd11fefb7346d89cd1ad22e07b68b10
wbtang-neuro <81193059+wbtang-neuro@users.noreply.github.com>
2026-08-13 14:55:19 -0400

    Update CA1RateMap_examples_Fig6bc.m

--- Figure6/CA1RateMap_examples_Fig6bc.m
+++ Figure6/CA1RateMap_examples_Fig6bc.m
@@ -3,7 +3,7 @@ clear all
 close all
 %% add codes to path
 addpath(genpath('C:\Users\Cornell\Documents\GitHub\neurocode')) % AYA lab neurocode
-addpath(genpath('C:\Users\Cornell\Documents\Drift_Cell_Tang2025\toolbox\matplotlib')) % colormap toolbox
+addpath(genpath('C:\Users\Cornell\Documents\drift_project\toolbox\matplotlib')) % colormap toolbox
 defaultGraphicsSetttings % graphic settings
 %% define data
 dir = 'U:\data\Zutshi_Neuron2021\CA3_mEC\'; % data folder
