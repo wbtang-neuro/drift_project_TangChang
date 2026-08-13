7fc275061764397100d128e15a319e720ac7a64b
wbtang-neuro <81193059+wbtang-neuro@users.noreply.github.com>
2026-08-13 14:57:09 -0400

    Update TimeAxis_CCGP_example_Fig3a_EDFig4j.m

--- Figure3/TimeAxis_CCGP_example_Fig3a_EDFig4j.m
+++ Figure3/TimeAxis_CCGP_example_Fig3a_EDFig4j.m
@@ -3,7 +3,7 @@ clear all
 close all
 %% add base codes to path
 addpath(genpath('C:\Users\Cornell\Documents\GitHub\neurocode')) % AYA lab neurocode
-addpath(genpath('C:\Users\Cornell\Documents\Drift_Cell_Tang2025\toolbox\matplotlib')) % colormap toolbox
+addpath(genpath('C:\Users\Cornell\Documents\drift_project\toolbox\matplotlib')) % colormap toolbox
 defaultGraphicsSetttings % graphics settings
 %% define data
 dir = 'W:\data\PPP\'; % data folder
