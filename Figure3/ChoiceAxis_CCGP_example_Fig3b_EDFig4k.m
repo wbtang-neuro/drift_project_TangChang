9b06bddd28551171ae0b6d6e14a6ab962d530f14
wbtang-neuro <81193059+wbtang-neuro@users.noreply.github.com>
2026-08-13 14:50:13 -0400

    Update ChoiceAxis_CCGP_example_Fig3b_EDFig4k.m

--- Figure3/ChoiceAxis_CCGP_example_Fig3b_EDFig4k.m
+++ Figure3/ChoiceAxis_CCGP_example_Fig3b_EDFig4k.m
@@ -3,8 +3,8 @@ clear all
 close all
 %% add base codes to path
 addpath(genpath('C:\Users\Cornell\Documents\GitHub\neurocode')) % AYA lab neurocode
-addpath(genpath('C:\Users\Cornell\Documents\Drift_Cell_Tang2025\toolbox\matplotlib')) % colormap toolbox
-addpath(genpath('C:\Users\Cornell\Documents\Drift_Cell_Tang2025\toolbox\libsvm-3.22')) % SVM toolbox
+addpath(genpath('C:\Users\Cornell\Documents\drift_project\toolbox\matplotlib')) % colormap toolbox
+addpath(genpath('C:\Users\Cornell\Documents\drift_project\toolbox\libsvm-3.22')) % SVM toolbox
 defaultGraphicsSetttings % graphics settings
 %% define data
 dir = 'W:\data\PPP\'; % data folder
